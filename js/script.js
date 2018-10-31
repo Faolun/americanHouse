adb93bf77f80ddd1a293b562e1be00e08381a4a9
Faolun <james.coatesiii@gmail.com>
2018-10-31 14:55:14 -0400

    skeleton work and email test

--- js/script.js
+++ js/script.js
@@ -1,19 +1,11 @@
 $(document).ready(function () {
   $('.sidenav').sidenav();
-//   $('.carousel.carousel-slider').carousel({
-//     fullWidth: true,
-//     duration: 150,
-//     indicators: true
-//   });
-//   autoplay();
-// function autoplay() {
-//     $('.carousel').carousel('next');
-//     setTimeout(autoplay, 6000);
-
-$('.slider').slider({
-  full_width: true,
-  interval: 5000
-});
+  $('.collapsible').collapsible();
+  $('.slider').slider({
+    full_width: true,
+    interval: 5000
+  });
+  
 
 });
 
